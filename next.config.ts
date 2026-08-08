a464cd060b9379c253aedcc6f77be85990eb4645
Hanif Fauzi <haniffauzihakim@gmail.com>
2026-08-08 23:49:20 +0700

    trigger rebuild

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  allowedDevOrigins: ['192.168.56.1', 'hanif-face-attend.my.id', '100.111.102.5']
+  allowedDevOrigins: ['192.168.56.1', 'hanif-face-attend.my.id', '100.111.102.5', '100.84.74.28']
 };
 
 export default nextConfig;
